bcaf2448bd7704423c9035b5162b9c3f13ecedd4
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2024-03-02 22:46:02 +0700

    revert blue back to white

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
     _getInitialInfo();
     return Scaffold(
       appBar: appBar(),
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.white,
       body: ListView(
         children: [
           _searchField(),
